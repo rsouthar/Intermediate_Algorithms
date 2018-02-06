2ec131dbd521e8653f54fc29d8e794e912b28998
Rick <rdsouthard1979@gmail.com>
2018-02-06 07:15:06 -0600

    finished with sum_range

--- sum_range.js
+++ sum_range.js
@@ -1,8 +1,21 @@
 function sumAll(arr) {
-  var max = Math.max(...arr);
-  var min = Math.min(...arr);
-  console.log(max);
-  return min;
+  //create a new array
+ var newArr = [];
 
+ //find the smallest and largest number in the array
+ var min = Math.min(arr[0], arr[1]);
+ var max = Math.max(arr[0], arr[1]);
+
+ //loop through the smallest and largest numbers and push them in to the new array
+ for (var i = min; i <= max; i++) {
+     newArr.push(i);
+ }
+
+ //use the reduce function to accumulate the totals of the new array and put it into a the variable sum
+ var sum = newArr.reduce(function(a, b) {
+     return a + b;
+ });
+//return the total
+ return sum;
 }
-sumAll([1, 4]);
+sumAll([10, 5]);
