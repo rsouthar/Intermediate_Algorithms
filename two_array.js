f7dd0407591c0cb6ed1bd02cb5228da57c1b13a9
Rick <rdsouthard1979@gmail.com>
2018-02-07 08:10:58 -0600

    finished with two_array.js

--- two_array.js
+++ two_array.js
@@ -1,7 +1,11 @@
 function diffArray(arr1, arr2) {
-  var newArr = [];
-  // Same, same; but different.
-  return newArr;
+  //arr1.filter() takes everything that is not in the second array
+  //and arr2.filter() takes everything that is not int he first array
+  //then .concat() is used to combine the two arrays
+  let diff = arr1.filter(x => !arr2.includes(x)).concat(arr2.filter(x => !arr1.includes(x)));
+  
+  return diff;
 }
 
-diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]);
+// output:  ['piglet', 4]
