ecfef980a8e7330b430454b3eb8fbaf62b9ca03a
Rick <rdsouthard1979@gmail.com>
2018-02-13 07:31:58 -0600

    started

---
+++ wherefore_art_thou.js
@@ -0,0 +1,14 @@
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+
+
+  // Only change code above this line
+  return arr;
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" },
+               { first: "Mercutio", last: null },
+               { first: "Tybalt", last: "Capulet" }],
+               { last: "Capulet" });
