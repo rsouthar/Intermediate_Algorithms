66c27031589b13af711434f227b86845a7d4e354
Rick <rdsouthard1979@gmail.com>
2018-02-23 16:20:09 -0600

    Finsished search_and_replace.js

--- search_and_replace.js
+++ search_and_replace.js
@@ -1,9 +1,16 @@
 function myReplace(str, before, after) {
-  var str1 = [str];
-  str1.splice(3, 0, before);
-  console.log(str1);
-
+  // Find the index of the before string in the str string and store it in newStr.
+  var newStr = str.indexOf(before);
+  // Test and see if teh string that is found is uppercased and if it is,
+  // uppper case the first letter of the variable after.
+  if (str[newStr] === str[newStr].toUpperCase()) {
+    after = after.chatAt(0).toUpperCase() + after.slice(1);
+  }
+  // Replace the string before with the string after.
+  str = str.replace(before, after);
+  // Return the result of str.
   return str;
+
 }
 
 myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
