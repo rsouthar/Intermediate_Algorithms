e09b4b6e6ca5e3b3b55de587aff79fe43339b565
Rick <rdsouthard1979@gmail.com>
2018-03-23 05:09:01 -0500

    Finished.

---
+++ smallest_common_multiple.js
@@ -0,0 +1,24 @@
+function smallestCommons(arr) {
+  var min = Math.min(...arr);
+  var max = Math.max(...arr);
+  var c = max;
+
+  var smallCommon = (function(low, high) {
+    function scm(l, h) {
+      if(h % l === 0) {
+        return h;
+      } else {
+        return scm(l, h + high);
+      }
+    }
+    return scm(low, high);
+  });
+  for ( var i = min; i <= max; i += 1) {
+    c = smallCommon(i, c);
+  }
+  console.log(c);
+  return c;
+}
+
+
+smallestCommons([1,5]);
